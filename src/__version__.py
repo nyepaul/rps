82df434b529d6b4b744ef82bacc3dcd5779653c2
Paul Nye <nyepaul@gmail.com>
2026-01-18 15:42:42 -0600

    chore: bump version to 3.7.2

--- src/__version__.py
+++ src/__version__.py
@@ -1,5 +1,5 @@
 """RPS Version Information"""
 
-__version__ = "3.7.1"
+__version__ = "3.7.2"
 __release_date__ = "2026-01-18"
-__release_notes__ = "Add profile cloning feature to duplicate profiles with all data"
+__release_notes__ = "Add spouse removal, fix analysis page layout, remove blue from calculated fields"
