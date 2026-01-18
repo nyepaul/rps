2310af3db346d4d5231083eacf37c31ff9ec33c4
Paul Nye <nyepaul@gmail.com>
2026-01-18 15:46:08 -0600

    chore: bump version to 3.7.3

--- src/__version__.py
+++ src/__version__.py
@@ -1,5 +1,5 @@
 """RPS Version Information"""
 
-__version__ = "3.7.2"
+__version__ = "3.7.3"
 __release_date__ = "2026-01-18"
-__release_notes__ = "Add spouse removal, fix analysis page layout, remove blue from calculated fields"
+__release_notes__ = "Add progress spinners for long-running operations (clone, save, load, delete)"
