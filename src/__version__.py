c4ce48ed70650966b0255315151b38c80c20284b
Paul Nye <nyepaul@gmail.com>
2026-01-21 15:57:38 -0600

    fix: resolve NameError 'json' in admin reports

--- src/__version__.py
+++ src/__version__.py
@@ -1,5 +1,5 @@
 """RPS Version Information"""
 
-__version__ = "3.8.117"
+__version__ = "3.8.118"
 __release_date__ = "2026-01-21"
-__release_notes__ = "Start Welcome tab with Guide collapsed"
+__release_notes__ = "Fix missing json import in admin reports"
