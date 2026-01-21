a2592260cdc79a8141e627f5cdcf179471180329
Paul Nye <nyepaul@gmail.com>
2026-01-21 17:16:00 -0600

    feat: change feedback button color to match standardized blue accent

--- src/__version__.py
+++ src/__version__.py
@@ -1,5 +1,5 @@
 """RPS Version Information"""
 
-__version__ = "3.8.125"
+__version__ = "3.8.126"
 __release_date__ = "2026-01-21"
-__release_notes__ = "Standardize blue colors across the application"
+__release_notes__ = "Standardize feedback button color"
