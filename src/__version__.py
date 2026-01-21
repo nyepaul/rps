af47974d35b7538f6a7d5014ad29542393847baa
Paul Nye <nyepaul@gmail.com>
2026-01-20 18:59:37 -0600

    chore: bump version to 3.8.90

--- src/__version__.py
+++ src/__version__.py
@@ -1,5 +1,5 @@
 """RPS Version Information"""
 
-__version__ = "3.8.89"
+__version__ = "3.8.90"
 __release_date__ = "2026-01-20"
-__release_notes__ = "fix: Eliminate flickering in audit log navigation by properly managing Leaflet map instances"
+__release_notes__ = "fix: Eliminate window flash in audit log navigation by updating only data container"
