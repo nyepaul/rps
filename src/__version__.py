2c3f6919b60c2615bfed2b673d77dbce8ebbef70
Paul Nye <nyepaul@gmail.com>
2026-01-22 14:00:42 -0600

    feat: realistic income and assets for demo profiles

--- src/__version__.py
+++ src/__version__.py
@@ -1,5 +1,5 @@
 """RPS Version Information"""
 
-__version__ = "3.8.158"
+__version__ = "3.8.159"
 __release_date__ = "2026-01-22"
-__release_notes__ = "Add typical expense categories (Entertainment, Charity, Subscriptions) to all demo profiles"
+__release_notes__ = "Fully populate all income and asset categories for both demo profiles"
