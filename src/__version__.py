a43d7e3dc2d4cf47ab52a0af0160d33d6549bb77
Paul Nye <nyepaul@gmail.com>
2026-01-22 12:32:14 -0600

    feat: admin reset-token command

--- src/__version__.py
+++ src/__version__.py
@@ -1,5 +1,5 @@
 """RPS Version Information"""
 
-__version__ = "3.8.145"
+__version__ = "3.8.146"
 __release_date__ = "2026-01-22"
-__release_notes__ = "Simplify recovery wizard by merging Email and Token options into Access Token category"
+__release_notes__ = "Add admin command to generate reset tokens"
