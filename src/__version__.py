6a6e067bb0feda26a22a525b2e08a71bb82f2fb6
Paul Nye <nyepaul@gmail.com>
2026-01-23 15:46:29 -0600

    chore: Release 3.9.4 - Admin UX Refactor

--- src/__version__.py
+++ src/__version__.py
@@ -1,5 +1,5 @@
 """RPS Version Information"""
 
-__version__ = "3.9.3"
+__version__ = "3.9.4"
 __release_date__ = "2026-01-23"
-__release_notes__ = "Enforce group-based permissions and bi-directional feedback replies"
+__release_notes__ = "Admin UX Refactoring and User Timeline Dropdown"
