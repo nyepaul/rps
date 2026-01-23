e0b515c86d708b4b75818b3e8d2c494da8355341
Paul Nye <nyepaul@gmail.com>
2026-01-23 17:01:19 -0600

    chore: bump version to 3.9.9

--- src/__version__.py
+++ src/__version__.py
@@ -1,5 +1,5 @@
 """RPS Version Information"""
 
-__version__ = "3.9.8"
+__version__ = "3.9.9"
 __release_date__ = "2026-01-23"
-__release_notes__ = "Make maps and charts theme-aware (responsive to normal/dark/high-contrast modes)"
+__release_notes__ = "Address comprehensive test failures: duplicate element IDs and bare except clauses"
